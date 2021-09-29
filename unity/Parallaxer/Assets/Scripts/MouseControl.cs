55b5ea0748f25fb1ec097d07c22ca135a368b415
Nick Fox-Gieg <nick@fox-gieg.com>
2021-09-28 23:27:26 -0600

    unity mouse control

---
+++ unity/Parallaxer/Assets/Scripts/MouseControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControl : MonoBehaviour {
+
+    public float lerpSpeed = 0.1f;
+
+	private void Start() {
+        
+    }
+
+    private void Update() {
+        Vector2 screenCoords = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = Vector2.Lerp(transform.position, new Vector2(screenCoords.x, transform.position.y), lerpSpeed);
+		if (screenCoords.x < 0f) {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        } else {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+    }
+
+}
